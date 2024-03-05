cbecf940f9b3ceaa9fdfe68302e9197726ea5cd0
Nahuel Luca <nahuelluca20@gmail.com>
2024-03-05 17:09:27 -0300

    Add links to navigate to different pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import {cookies} from "next/headers";
+import Link from "next/link";
 
 export default function Page() {
-  cookies(); // headers() noStore()
-
   return (
     <div>
       <h1>App Router</h1>
+      <Link href={"/jobs?page=2"} style={{padding: "8px", marginTop: "5px"}}>
+        Go to Page 2
+      </Link>
+      <Link href={"/jobs?page=5"} style={{padding: "8px", marginTop: "5px"}}>
+        Go to Page 5
+      </Link>
+
       <p>{Date.now()}</p>
     </div>
   );
